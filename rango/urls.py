3bd2ee15423c71be9d1ec61008aa42ad3c4bffb4
Stephen Scott <scott.stephen54@yahoo.co.uk>
2019-02-22 16:33:38 +0000

    Chapter 11

--- rango/urls.py
+++ rango/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import url
 from rango import views
 
@@ -9,8 +10,7 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     url(r'^restricted/', views.restricted, name='restricted'),
-    url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^register/$',
-        views.register, name='register'), # New pattern!
+    # url(r'^logout/$', views.user_logout, name='logout'),
+    # url(r'^login/$', views.user_login, name='login'),
+    # url(r'^register/$', views.register, name='register'),  # New pattern!
 ]
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -14,14 +14,49 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# from django.conf.urls import url
+# from django.contrib import admin
+# from django.conf.urls import include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from rango import views
+# from registration.backends.simple.views import RegistrationView
+#
+#
+# class MyRegistrationView(RegistrationView):
+#     def get_success_url(self, user):
+#         return '/rango/'
+#
+#
+# urlpatterns = [
+#     url(r'^$', views.homepage, name='index'),
+#     url(r'^rango/', include('rango.urls')),
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+#     url(r'^accounts/', include('registration.backends.simple.urls')),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page,
+#if successful at logging
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
+
+
 from rango import views
 urlpatterns = [
+url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+url(r'^accounts/', include('registration.backends.simple.urls')),
 url(r'^$', views.index, name='index'),
 url(r'^rango/', include('rango.urls')),
 # above maps any URLs starting
